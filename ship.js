ea3c09d01b0192d76561b955bbfbe2e9aa042279
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2025-02-20 15:31:53 -0500

    initial setup and ship class

---
+++ ship.js
@@ -0,0 +1,18 @@
+class Ship {
+  constructor(shipLength, hitCount = 0) {
+    this.shipLength = shipLength;
+    this.hitCount = hitCount;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hitCount += 1;
+  }
+
+  isSunk() {
+    if (this.hitCount >= this.shipLength) {
+      this.sunk = true;
+    }
+    return this.sunk;
+  }
+}
